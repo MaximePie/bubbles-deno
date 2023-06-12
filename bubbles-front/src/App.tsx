e0f5d639454b2e977f18ffe0bb070f30b84e4e49
Maxime Pie <maxime.pie.mail@gmail.com>
2023-06-12 06:54:34 +0200

    feat: add first endpoint

--- bubbles-front/src/App.tsx
+++ bubbles-front/src/App.tsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * A random bubble to be added to a project
+ */
+type DraftBubble = {
+  name: string;
+}
 
+type DraftProject = {
+  name: string;
+  bubbles: DraftBubble[];
+}
+export function ProjectForm() {
+  const [draftProject, setDraftProject] = useState<DraftProject>({
+    name: '',
+    bubbles: [],
+  });
+  const [draftBubble, setDraftBubble] = useState<DraftBubble>({
+    name: '',
+  });
+  return (
+    <div>
+      <h1>Project Form</h1>
+      <h4>Name</h4>
+      <input
+        type="text"
+        value={draftProject.name}
+onChange={(e) => setDraftProject({ ...draftProject, name: e.target.value })}
+      />
+      <h4>Bubbles</h4>
+      <input type="text" value={draftBubble.name} onChange={(e) => setDraftBubble({ ...draftBubble, name: e.target.value })}/>
+      <button onClick={addBubble}>
+        Add Bubble
+      </button>
+      <button
+        onClick={saveProject}
+      >
+        Save
+      </button>
+      <ul>
+        {draftProject.bubbles.map((bubble) => (
+          <li key={bubble.name}>{bubble.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+
+  function addBubble() {
+    setDraftProject({
+      ...draftProject,
+      bubbles: [...draftProject.bubbles, draftBubble],
+    })
+    setDraftBubble({ name: '' })
+  }
+
+  /**
+   * Send data to server and save project
+   */
+  function saveProject() {
+    axios.post('http://localhost:3001/projects', draftProject)
+  }
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR AND PUSH THIS
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ProjectForm/>
     </>
   )
 }
--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,14 @@
-import express from "https://esm.sh/express@4.18.2";
+import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
 
-const app = express();
 
+const app = new expressive.App();
+app.use(expressive.simpleLog());
+app.use(expressive.bodyParser.json());
 app.get("/", (req, res) => {
   res.json({message: "Welcome to the Dinosaur API!"});
+});
+app.post("/project", (req, res) => {
+  res.json({message: "Project has been posted!", project: JSON.stringify(req.data)});
 });
 
 app.listen(8000);
